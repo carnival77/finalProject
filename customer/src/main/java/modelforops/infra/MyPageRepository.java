d45283036c7dd101a256fd49b7c8d13ecbec50b2
Carnival77 test <stam0325@gmail.com>
2024-04-04 04:56:33 +0000

    bf test 240404

--- customer/src/main/java/modelforops/infra/MyPageRepository.java
+++ customer/src/main/java/modelforops/infra/MyPageRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "myPages", path = "myPages")
 public interface MyPageRepository
-    extends PagingAndSortingRepository<MyPage, Long> {}
+    extends PagingAndSortingRepository<MyPage, Long> {
+        java.util.Optional<MyPage> findByOrderId(Long id);
+    }
